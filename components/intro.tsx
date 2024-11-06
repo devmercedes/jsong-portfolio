9599e6a6d2ad4890311860b2180b8d8b6cb6d631
Wesley Wang <wesleywang4766@gmail.com>
2024-11-06 14:40:07 +0900

    fix: update linkedin and github url

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,7 @@ export default function Intro() {
       >
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack text-gray-950"
-          href="https://github.com/ben04rogers"
+          href="https://github.com/SuperFireCoder"
           target="_blank"
         >
           GitHub <FaGithub className="opacity-70" />
@@ -86,7 +86,7 @@ export default function Intro() {
 
         <a
           className="group bg-gray-950 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition border-2 border-white border-opacity-40"
-          href="https://www.linkedin.com/in/ben-rogers-dev/"
+          href="https://www.linkedin.com/in/joseph-song-994662325/"
           target="_blank"
         >
           <span className="opacity-70">LinkedIn</span>
